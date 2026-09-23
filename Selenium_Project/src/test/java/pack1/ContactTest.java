d27e9d69a74eb516fad45122715bc1c07fd4b44a
SaiBhanuprakash <saivazrapu23@gmail.com>
2026-09-23 15:51:32 +0530

    I updated test cases

--- Selenium_Project/src/test/java/pack1/ContactTest.java
+++ Selenium_Project/src/test/java/pack1/ContactTest.java
@@ -23,8 +23,4 @@ public class ContactTest {
 	public void modifycontactTest() {
 		System.out.println("execute modifycontactTest");
 	}
-	@Test
-	public void deletecontactTest() {
-		System.out.println("execute deletecontactTest");
-	}
 }
